54cb78407cf468846a7123c0e6fa41cea2e4eb85
Selim Boumedien <selim.boumedien@hotmail.fr>
2023-01-09 17:45:22 +0100

    api page produit relier et affichage dynamique de la page , manque choix couleurs

--- front/js/product.js
+++ front/js/product.js
@@ -1,32 +1,47 @@
-fetch("http://localhost:3000/api/products/107fb5b75607497b96722bda5b504926")
+
+let params = new URL(document.location).searchParams;
+let id = params.get("id");
+console.log(id);
+
+fetch("http://localhost:3000/api/products")
 
 .then((res) =>  res.json())
 
   .then((lesProduits) => {
-     console.table(lesProduits);
      lesKanaps(lesProduits);
   } )
   
   .catch((err) => {
     // Une erreur est survenue
   });
-  /*
 
-  let urlData = new URL (http://localhost:3000/api/products/107fb5b75607497b96722bda5b504926)
-*/
-/*
-  function lesKanaps(index) {
 
-    let affichage = document.querySelector("#items") ;
 
-    for (let article of index ) {
+  function lesKanaps(product) {
+
+        let affichage = document.querySelector("#item") ;
+        let imageAlt = document.querySelector("article div.item__img")
+        let titre = document.querySelector("#title")
+        let description = document.querySelector("#description")
+        let prix = document.querySelector("#price")
+        /* let color = document.querySelector("#colors") */
+
+    for (let article of product) {
+
+        if (id === article._id) {
         
-        affichage.innerHTML += `<a href="./product.html?id=${article._id107fb5b75607497b96722bda5b504926}">
-        <article>
-          <img src="${article.imageUrl}" alt="${article.altTxt}">
-          <h3 class="productName">${article.name}</h3>
-          <p class="productDescription">${article.description}</p>
-        </article>
-      </a>`
-    }
-  }*/
+            imageAlt.innerHTML = `<img src="${article.imageUrl}" alt="${article.altTxt}">`;
+
+            titre.textContent = `${article.name}`;
+
+            prix.textContent = `${article.price}`;
+
+            description.textContent = `${article.description}`;
+         
+          }
+        }
+        console.log("affichage ok");
+        }
+    
+  
+  
