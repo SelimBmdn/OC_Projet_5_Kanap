9a2347e7514c9bca4564377c7c7c9c8aa0115e27
Selim Boumedien <selim.boumedien@hotmail.fr>
2023-01-30 09:53:08 +0100

    gestion formulaire ok

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,19 +1,23 @@
-
-fetch("http://localhost:3000/api/products")
-
-  .then((res) => res.json())
-
-  .then((lesProduits) => {
-    affichageBasket(lesProduits);
-  })
-
-  .catch((err) => {
-    // Une erreur est survenue
-  });
+// Page panier
+const page = document.location.href;
+//Récupération des produits de l'api
+if (page.match("cart")) {
+  fetch("http://localhost:3000/api/products")
+    .then((res) => res.json())
+    .then((lesProduits) => {
+      console.log(lesProduits);
+      affichageBasket(lesProduits);
+    })
+    .catch((err) => {
+      //msg erreur
+    });
+} else {
+  console.log("sur page confirmation");
+}
 
 
 function affichageBasket(index) {
- 
+
   let basket = JSON.parse(localStorage.getItem("basket"));
 
   // si il y a un panier avec une taille differante de 0 (donc supérieure à 0)
@@ -77,10 +81,10 @@ function affiche(indexé) {
 
   // reste à l'écoute des modifications de quantité pour l'affichage et actualiser les données
   totalProduit();
- /* suppression();*/
+  /* suppression();*/
   getNumberProduct();
   getTotalPrice();
-  
+
 }
 
 // modif quantité exemple correction
@@ -119,12 +123,15 @@ function modifQuantité() {
 
 
 // Total produit et cout total
+/*
+let prixTotalCalcul = [];
+
+for (let m = 0; m < basket.length; m++){
+  console.log(basket[m].prix);
+}
 
 
-
-
-
-//--------------"Correction"
+*/
 
 
 
@@ -132,7 +139,7 @@ function modifQuantité() {
 
 function saveBasket(basket) {
   localStorage.setItem("basket", JSON.stringify(basket));
-} ;
+};
 
 
 function getBasket() {
@@ -142,14 +149,14 @@ function getBasket() {
   } else {
     return JSON.parse(basket);
   }
-} ;
+};
 
 function addBasket(product, quantity) {
   let basket = getBasket();
   let foundProduct = basket.find(p => p._id == product._id);
   if (foundProduct != undefined) {
     foundProduct.quantity = parseInt(quantity) + parseInt(foundProduct.quantity);
-    
+
   } else {
     product.quantity = quantity;
     basket.push(product);
@@ -157,22 +164,71 @@ function addBasket(product, quantity) {
 
   saveBasket(basket);
 
-} ;
+};
+
+// supprimer produit du panier
+
 
 function removeFromBasket(product) {
+  let basket = getBasket();
+  basket = basket.filter(p => p._id != product._id);
+  saveBasket(basket);
 
-let basket = getBasket();
-basket = basket.filter(p => p._id != product._id);
+};
+/*
+ document.getElementsByClassName("deleteItem").addEventListener("click",function(){
+  alert ('deleteItem');
+})
+;
+
+*/
+
+
+
+
+/*
+let btnDelete = document.querySelectorAll (".cart__item .deleteItem");
+btnDelete.forEach((btnDelete) => {
+  btnDelete.addEventListener("click" , () => {
+    let panier = JSON.parse(localStorage.getItem("basket"));
+    for (let d = 0, c panier.length; d < c; d++)
+    if (
+      basket[d]._id === btnDelete.dataset.id &&
+      basket[d].couleur === btnDelete.dataset.couleur
+    ) {
+      const num [d] ;
+      let newBasket = JSON.parse(localStorage.getItem("basket"));
+      newBasket.splice(num, 1);
+
+      if (newBasket && newBasket.length == 0) {
+        document.querySelector("totalQuantity").innerHTML = "0" ;
+        document.querySelector("totalPrice").innerHTML = "0" ;
+        document.querySelector("h1").innerHTML = "Panier vide" ;
+
+      }
+      localStorage.basket = JSON.stringify(newBasket);
+      totalProduit();
+
+      return location.reload();
+    }
+
+  });
+
+});
+/*basket = basket.filter(p => p._id != product._id);
 saveBasket(basket);
-  } ;
-
+  } 
+  
+  
+  ;
+*/
 
 
 function changeQuantity(product, quantity) {
   let basket = getBasket();
   let foundProduct = basket.find(p => p._id == product._id);
   if (foundProduct != undefined) {
-    foundProduct.quantity += quantity;
+    foundProduct.quantity = parseInt(quantity) + parseInt(foundProduct.quantity);
     if (foundProduct.quantity <= 0) {
       removeFromBasket(foundProduct);
     } else {
@@ -190,8 +246,8 @@ function getNumberProduct() {
   }
   return number;
 
- 
-} ;
+
+};
 
 
 function getTotalPrice() {
@@ -201,6 +257,307 @@ function getTotalPrice() {
     total += product.quantity * product.prix;
   }
   return total;
-} ;
+};
 
 
+// gestion formulaire 
+
+//const cart = document.querySelectorAll(".cart__item");
+
+// les données du client seront stockées dans ce tableau pour la commande sur page panier
+if (page.match("cart")) {
+  var contactClient = {};
+  localStorage.contactClient = JSON.stringify(contactClient);
+  // voir https://cheatography.com/davechild/cheat-sheets/regular-expressions/
+  /* regex email stackoverflow (?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\]) */
+  /* équivalent en javascript à  	
+  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ */
+  // équivalent pour w3c /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+  // on pointe des éléments input, on attribut à certains la même classe, ils régiront pareil aux différantes regex
+  // on pointe les input nom prénom et ville
+  var prenom = document.querySelector("#firstName");
+  prenom.classList.add("regex_texte");
+  var nom = document.querySelector("#lastName");
+  nom.classList.add("regex_texte");
+  var ville = document.querySelector("#city");
+  ville.classList.add("regex_texte");
+  // on pointe l'input adresse
+  var adresse = document.querySelector("#address");
+  adresse.classList.add("regex_adresse");
+  // on pointe l'input email
+  var email = document.querySelector("#email");
+  email.classList.add("regex_email");
+  // on pointe les élément qui ont la classe .regex_texte
+  var regexTexte = document.querySelectorAll(".regex_texte");
+  // modification du type de l'input type email à text à cause d'un comportement de l'espace blanc non voulu vis à vis de la regex 
+  document.querySelector("#email").setAttribute("type", "text");
+};
+
+//regex 
+// /^ début regex qui valide les caratères
+let regexLettre = /^[a-záàâäãåçéèêëíìîïñóòôöõúùûüýÿæœ\s-]{1,31}$/i;
+// /^ début regex qui valide les caratères chiffre lettre et caratères spéciaux   
+let regexChiffreLettre = /^[a-z0-9áàâäãåçéèêëíìîïñóòôöõúùûüýÿæœ\s-]{1,60}$/i;
+let regValideEmail = /^[a-z0-9æœ.!#$%&’*+/=?^_`{|}~"(),:;<>@[\]-]{1,60}$/i;
+let regMatchEmail = /^[a-zA-Z0-9æœ.!#$%&’*+/=?^_`{|}~"(),:;<>@[\]-]+@([\w-]+\.)+[\w-]{2,4}$/i;
+
+// Ecoute et attribution de point(pour sécurité du clic) si ces champs sont ok d'après la regex
+
+if (page.match("cart")) {
+  regexTexte.forEach((regexTexte) =>
+    regexTexte.addEventListener("input", (e) => {
+      // valeur sera la valeur de l'input en dynamique
+      valeur = e.target.value;
+      // regNormal sera la valeur de la réponse regex, 0 ou -1
+      let regNormal = valeur.search(regexLettre);
+      if (regNormal === 0) {
+        contactClient.firstName = prenom.value;
+        contactClient.lastName = nom.value;
+        contactClient.city = ville.value;
+      }
+      if (
+        contactClient.city !== "" &&
+        contactClient.lastName !== "" &&
+        contactClient.firstName !== "" &&
+        regNormal === 0
+      ) {
+        contactClient.regexNormal = 3;
+      } else {
+        contactClient.regexNormal = 0;
+      }
+      localStorage.contactClient = JSON.stringify(contactClient);
+      couleurRegex(regNormal, valeur, regexTexte);
+      valideClic();
+    })
+  );
+}
+// le champ écouté via la regex regexLettre fera réagir, grâce à texteInfo, la zone concernée
+texteInfo(regexLettre, "#firstNameErrorMsg", prenom);
+texteInfo(regexLettre, "#lastNameErrorMsg", nom);
+texteInfo(regexLettre, "#cityErrorMsg", ville);
+// Ecoute et attribution de point(pour sécurité du clic) si ces champs sont ok d'après la regex
+if (page.match("cart")) {
+  let regexAdresse = document.querySelector(".regex_adresse");
+  regexAdresse.addEventListener("input", (e) => {
+    // valeur sera la valeur de l'input en dynamique
+    valeur = e.target.value;
+    // regNormal sera la valeur de la réponse regex, 0 ou -1
+    let regAdresse = valeur.search(regexChiffreLettre);
+    if (regAdresse == 0) {
+      contactClient.address = adresse.value;
+    }
+    if (contactClient.address !== "" && regAdresse === 0) {
+      contactClient.regexAdresse = 1;
+    } else {
+      contactClient.regexAdresse = 0;
+    }
+    localStorage.contactClient = JSON.stringify(contactClient);
+    couleurRegex(regAdresse, valeur, regexAdresse);
+    valideClic();
+  });
+}
+//------------------------------------
+// le champ écouté via la regex regexChiffreLettre fera réagir, grâce à texteInfo, la zone concernée
+texteInfo(regexChiffreLettre, "#addressErrorMsg", adresse);
+// Ecoute et attribution de point(pour sécurité du clic) si ce champ est ok d'après les regex
+if (page.match("cart")) {
+  let regexEmail = document.querySelector(".regex_email");
+  regexEmail.addEventListener("input", (e) => {
+    // valeur sera la valeur de l'input en dynamique
+    valeur = e.target.value;
+    // https://webdevdesigner.com/q/what-characters-are-allowed-in-an-email-address-65767/ mon adresse doit avoir cette forme pour que je puisse la valider
+    let regMatch = valeur.match(regMatchEmail);
+    // quand le resultat sera correct, le console log affichera une autre réponse que null; regValide sera la valeur de la réponse regex, 0 ou -1
+    let regValide = valeur.search(regValideEmail);
+    if (regValide === 0 && regMatch !== null) {
+      contactClient.email = email.value;
+      contactClient.regexEmail = 1;
+    } else {
+      contactClient.regexEmail = 0;
+    }
+    localStorage.contactClient = JSON.stringify(contactClient);
+    couleurRegex(regValide, valeur, regexEmail);
+    valideClic();
+  });
+}
+// texte sous champ email
+if (page.match("cart")) {
+  email.addEventListener("input", (e) => {
+    // valeur sera la valeur de l'input en dynamique
+    valeur = e.target.value;
+    let regMatch = valeur.match(regMatchEmail);
+    let regValide = valeur.search(regValideEmail);
+    // si valeur est toujours un string vide et la regex différante de 0 (regex à -1 et le champ est vide mais pas d'erreur)
+    if (valeur === "" && regMatch === null) {
+      document.querySelector("#emailErrorMsg").textContent = "Veuillez renseigner votre email.";
+      document.querySelector("#emailErrorMsg").style.color = "white";
+      // si valeur n'est plus un string vide et la regex différante de 0 (regex à -1 et le champ n'est pas vide donc il y a une erreur)
+    } else if ( regValide !== 0) {
+      document.querySelector("#emailErrorMsg").innerHTML = "Caractère non valide";
+      document.querySelector("#emailErrorMsg").style.color = "white";
+      // pour le reste des cas (quand la regex ne décèle aucune erreur et est à 0 peu importe le champ vu qu'il est validé par la regex)
+    } else if (valeur != "" && regMatch == null) {
+      document.querySelector("#emailErrorMsg").innerHTML = "Caratères acceptés pour ce champ. Forme email pas encore conforme";
+      document.querySelector("#emailErrorMsg").style.color = "white";
+    } else {
+      document.querySelector("#emailErrorMsg").innerHTML = "Forme email conforme.";
+      document.querySelector("#emailErrorMsg").style.color = "white";
+    }
+  });
+}
+// fonction couleurRegex qui modifira la couleur de l'input par remplissage tapé, aide visuelle et accessibilité
+// on détermine une valeur de départ à valeur qui sera un string
+let valeurEcoute = "";
+// fonction à 3 arguments réutilisable, la regex, la valeur d'écoute, et la réponse à l'écoute
+function couleurRegex(regSearch, valeurEcoute, inputAction) {
+  // si valeur est toujours un string vide et la regex différante de 0 (regex à -1 et le champ est vide mais pas d'erreur)
+  if (valeurEcoute === "" && regSearch != 0) {
+    inputAction.style.backgroundColor = "white";
+    inputAction.style.color = "black";
+    // si valeur n'est plus un string vide et la regex différante de 0 (regex à -1 et le champ n'est pas vide donc il y a une erreur)
+  } else if (valeurEcoute !== "" && regSearch != 0) {
+    inputAction.style.backgroundColor = "rgb(220, 50, 50)";
+    inputAction.style.color = "white";
+    // pour le reste des cas (quand la regex ne décèle aucune erreur et est à 0 peu importe le champ vu qu'il est validé par la regex)
+  } else {
+    inputAction.style.backgroundColor = "rgb(0, 138, 0)";
+    inputAction.style.color = "white";
+  }
+}
+// fonction d'affichage individuel des paragraphes sous input sauf pour l'input email
+function texteInfo(regex, pointage, zoneEcoute) {
+      if (page.match("cart")) {
+      zoneEcoute.addEventListener("input", (e) => {
+      // valeur sera la valeur de l'input en dynamique
+      valeur = e.target.value;
+      index = valeur.search(regex);
+    // si valeur est toujours un string vide et la regex différante de 0 (regex à -1 et le champ est vide mais pas d'erreur)
+      if (valeur === "" && index != 0) {
+        document.querySelector(pointage).textContent = "Veuillez renseigner ce champ.";
+        document.querySelector(pointage).style.color = "white";
+        // si valeur n'est plus un string vide et la regex différante de 0 (regex à -1 et le champ n'est pas vide donc il y a une erreur)
+      } else if (valeur !== "" && index != 0) {
+        document.querySelector(pointage).innerHTML = "Reformulez cette donnée";
+        document.querySelector(pointage).style.color = "white";
+        // pour le reste des cas (quand la regex ne décèle aucune erreur et est à 0 peu importe le champ vu qu'il est validé par la regex)
+      } else {
+      document.querySelector(pointage).innerHTML = "Caratères acceptés pour ce champ.";
+      document.querySelector(pointage).style.color = "white";
+      }
+    });
+  }
+}
+//--------------------------------------------------------------
+// Fonction de validation/d'accés au clic du bouton du formulaire
+//--------------------------------------------------------------
+let commande = document.querySelector("#order");
+// la fonction sert à valider le clic de commande de manière interactive
+function valideClic() {
+  let contactRef = JSON.parse(localStorage.getItem("contactClient"));
+  let somme =
+    contactRef.regexNormal + contactRef.regexAdresse + contactRef.regexEmail;
+  if (somme === 5) {
+    commande.removeAttribute("disabled", "disabled");
+    document.querySelector("#order").setAttribute("value", "Commander !");
+  } else {
+    commande.setAttribute("disabled", "disabled");
+    document.querySelector("#order").setAttribute("value", "Remplir le formulaire");
+  }
+}
+//----------------------------------------------------------------
+// Envoi de la commande
+//----------------------------------------------------------------
+if (page.match("cart")) {
+  commande.addEventListener("click", (e) => {
+    // empeche de recharger la page on prévient le reload du bouton
+    e.preventDefault();
+    valideClic();
+    envoiPaquet();
+  });
+}
+//----------------------------------------------------------------
+// fonction récupérations des id puis mis dans un tableau
+//----------------------------------------------------------------
+// définition du panier quine comportera que les id des produits choisi du local storage
+let panierId = [];
+function tableauId() {
+// appel des ressources
+let panier = JSON.parse(localStorage.getItem("basket"));
+// récupération des id produit dans panierId
+if (panier && panier.length > 0) {
+  for (let indice of panier) {
+    panierId.push(indice._id);
+  }
+} else {
+  console.log("le panier est vide");
+  document.querySelector("#order").setAttribute("value", "Panier vide!");
+}
+}
+//----------------------------------------------------------------
+// fonction récupération des donnée client et panier avant transformation
+//----------------------------------------------------------------
+let contactRef;
+let commandeFinale;
+function paquet() {
+  contactRef = JSON.parse(localStorage.getItem("contactClient"));
+  // définition de l'objet commande
+  commandeFinale = {
+    contact: {
+      firstName: contactRef.firstName,
+      lastName: contactRef.lastName,
+      address: contactRef.address,
+      city: contactRef.city,
+      email: contactRef.email,
+    },
+    products: panierId,
+  };
+}
+//----------------------------------------------------------------
+// fonction sur la validation de l'envoi
+//----------------------------------------------------------------
+function envoiPaquet() {
+  tableauId();
+  paquet();
+  // vision sur le paquet que l'on veut envoyer
+  console.log(commandeFinale);
+  let somme = contactRef.regexNormal + contactRef.regexAdresse + contactRef.regexEmail;
+  // si le panierId contient des articles et que le clic est autorisé
+  if (panierId.length != 0 && somme === 5) {
+    // envoi à la ressource api
+    fetch("http://localhost:3000/api/products/order", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(commandeFinale),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        // envoyé à la page confirmation, autre écriture de la valeur "./confirmation.html?commande=${data.orderId}"
+        window.location.href = `/front/html/confirmation.html?commande=${data.orderId}`;
+      })
+      .catch(function (err) {
+        console.log(err);
+        alert("erreur");
+      });
+  }
+}
+//------------------------------------------------------------
+// fonction affichage autoinvoquée du numéro de commande et vide du storage lorsque l'on est sur la page confirmation
+//------------------------------------------------------------
+(function Commande() {
+  if (page.match("confirmation")) {
+    sessionStorage.clear();
+    localStorage.clear();
+    // valeur du numero de commande
+    let numCom = new URLSearchParams(document.location.search).get("commande");
+    // merci et mise en page
+    document.querySelector("#orderId").innerHTML = `<br>${numCom}<br>Merci pour votre achat`;
+    console.log("valeur de l'orderId venant de l'url: " + numCom);
+    //réinitialisation du numero de commande
+    numCom = undefined;
+  } else {
+    console.log("sur page cart");
+  }
+})();
